7dc44950dbbe17fba4bb3f7517a1b304ac69e802
Bagagroup <thebagagroup@gmail.com>
2016-10-02 17:05:57 +0600

    Update AppDelegate.swift

--- ZhelAyaq/AppDelegate.swift
+++ ZhelAyaq/AppDelegate.swift
@@ -23,7 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
 
-//REFrostedViewController application
+//REFrostedViewController
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         let homeViewController = UINavigationController(rootViewController: ViewController()) //создания navigationBar
